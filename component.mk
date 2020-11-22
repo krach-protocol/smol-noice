dca0afe764b6134520544e7598496406332eaacd
seriousd <dennis.ernst@citynode.de>
2020-11-22 22:34:06 +0100

    modify Makefiles for libary use

--- component.mk
+++ component.mk
@@ -1,24 +1,8 @@
 COMPONENT_SRCDIRS := 	src \
 						libs/tinycbor/src\
-						libs/smolcert-esp32/src \
-						libs/noise-c/src \
-#						libs/noise-c/src/protocol \
-#						libs/noise-c/src/crypto/* \
-#						libs/noise-c/src/backend/ref \
-#						libs/noise-c/src/crypto/goldilocks/src \
-#						libs/noise-c/src/crypto/goldilocks/src/* \
+						libs/smolcert-esp32/src 
 COMPONENT_ADD_INCLUDEDIRS := 	inc \
-								libs/smolcert-esp32/include \
-								libs/noise-c/include/ 
+								libs/smolcert-esp32/include 
 COMPONENT_PRIV_INCLUDEDIRS := 	libs/tinycbor/src \
-								libs/smolcert-esp32/include \
-								libs/noise-c/include
-#								libs/noise-c/include/keys \
-#								libs/noise-c/include/protocol \
-#								libs/noise-c/src/protocol \
-#								libs/noise-c/include/protocol \
-#								libs/noise-c/src/ \
-#								libs/noise-c/src/crypto/goldilocks/src/include \
-#								libs/noise-c/src/crypto/goldilocks/src/p448/ \
-#								libs/noise-c/src/crypto/goldilocks/src/p448/arch_32 
+								libs/smolcert-esp32/include 
 COMPONENT_DEPENDS := libdosium
