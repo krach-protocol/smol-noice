3ed20d4b519723f9277abcb591839fcfb833fee3
seriousd <dennis.ernst@citynode.de>
2021-03-10 16:04:29 +0100

    add receive task and add proper queue handling

--- src/port/Linux/port_Linux.c
+++ src/port/Linux/port_Linux.c
@@ -7,6 +7,7 @@
 #include <arpa/inet.h> 
 
 #include <pthread.h> 
+#include <errno.h>
 
 
 
@@ -19,9 +20,14 @@
 
 
 #define QUEUE_LEN 10
+#define RX_BUFLEN 255
 queue_t *rxQueue;
 int sock;
+pthread_mutex_t lock;
 
+
+
+void* socketListenerTask(void*);
 
 void startTask(void* (*workerTask)(void*),void* args){
     pthread_t tid; 
@@ -54,7 +60,7 @@ uint8_t openSocket(char* addr,uint16_t port){
     printf("Connecting to: %s:%d\n",addr,port);
    if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-       printf(" Error : Connect Failed \n");
+       printf("Error : Connect Failed \n");
        return 1;
     } 
     printf("Connect ok\n");
@@ -65,7 +71,38 @@ uint8_t openSocket(char* addr,uint16_t port){
     }
     printf("Init Queue ok\n");
 
+    pthread_mutex_init(&lock, NULL);
+    startTask(socketListenerTask,(void*)rxQueue);    
     return 0;
+}
+
+void* socketListenerTask(void* args){
+    queue_t* rxQueue = (queue_t*)args;
+    uint8_t bytesRead = 0;
+    uint8_t rxBuffer[RX_BUFLEN];
+    int16_t readResult  = 0;
+    sn_msg_t* rxMsg;
+    int16_t errsv;
+    while(1){
+        sleep_ms(100);
+         readResult = recv(sock, rxBuffer + bytesRead, RX_BUFLEN - bytesRead,MSG_DONTWAIT);
+         if(readResult < 0){
+            errsv = errno;
+            //printf("Error on socket: %s\n",strerror(errno));
+         } else if(readResult > 0){
+             printf("Read %d bytes from socket\n",readResult);
+             bytesRead += readResult;
+             //Rx complete
+             rxMsg = (sn_msg_t*)malloc(sizeof(sn_msg_t));
+             rxMsg->msgLen = bytesRead;
+             rxMsg->msgBuf = (uint8_t*)malloc(rxMsg->msgLen);
+             memcpy(rxMsg->msgBuf,rxBuffer,rxMsg->msgLen);
+            pthread_mutex_lock(&lock);
+             addToQueue(rxQueue,rxMsg);
+              pthread_mutex_unlock(&lock);
+             bytesRead = 0;
+         } 
+    } 
 }
 
 void sendOverNetwork(sn_msg_t* msg){
@@ -78,12 +115,20 @@ void sendOverNetwork(sn_msg_t* msg){
 uint8_t messageFromNetwork(sn_msg_t* msg){
     sn_msg_t* data = NULL;
     uint8_t ret = 0;
-    if(messageInQueue(rxQueue)){
-        getMessageFromQueue(rxQueue,data); //TODO: Error handling
-        memcpy((void*)msg,data,sizeof(sn_msg_t*));
-        free(data);
+    pthread_mutex_lock(&lock);
+
+    if(messageInQueue(rxQueue) == DATA_AVAILIBLE){
+        getMessageFromQueue(rxQueue,&data); //TODO: Error handling
+        msg->msgLen = data->msgLen;
+        free(msg->msgBuf);
+
+        msg->msgBuf = (uint8_t*)malloc(msg->msgLen);
+        memcpy(msg->msgBuf,data->msgBuf,msg->msgLen);
+
+        //free(data);
         ret = 1;
     }
+     pthread_mutex_unlock(&lock);
 
     return ret;
 }
