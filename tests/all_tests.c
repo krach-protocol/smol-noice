148efb2752413ee150e570e4fc7381e340599378
Till Klocke <till.klocke@gmail.com>
2021-05-05 13:04:08 +0200

    Added test to ensure uint16 are read correctly from packets

--- tests/all_tests.c
+++ tests/all_tests.c
@@ -20,6 +20,7 @@ void test_makeNoiseHandshake(void);
 void test_packHandshakeInit(void);
 void test_unpackHandshakeResponse(void);
 void test_packHandshakeFin(void);
+void test_readUint16(void);
 
 sc_error_t loadSmolCert(const char*,smolcert_t**);
 
@@ -39,6 +40,12 @@ sc_error_t loadSmolCert(const char*,smolcert_t**);
 
 #define RESPONSE_PACKET_VERSION 0x01
 #define RESPONSE_PACKET_TYPE HANDSHAKE_RESPONSE
+
+void test_readUint16(void) {
+  uint8_t testInt[] = {0xE9,0x07};
+  uint16_t i = readUint16((uint8_t*)&testInt);
+  TEST_ASSERT_EQUAL_MESSAGE(2025, i, "Failed to read little endian integer from byte array");
+}
 
 void test_packHandshakeFin(void){
   uint8_t handshakeTestVektor[] = {INIT_PACKET_LEN,FIN_PACKET_VERSION,FIN_PACKET_TYPE,DUMMY_PUBKEY};
@@ -79,24 +86,26 @@ void test_unpackHandshakeResponse(void){
   srand((unsigned) time(&t));
 
   //Craft test message
-  uint16_t messageLen = 34; //length of packetLen not included 
-  uint16_t payloadLen = (16-messageLen%16); //Filling up the payload to divisible by 16 len, for testing
-  uint16_t totalLen = messageLen + payloadLen; 
+  uint16_t messageLen = 66; //length of packetLen not included 
+  uint16_t smolcertLen = 32; //Filling up the payload to divisible by 16 len, for testing
+  uint16_t totalLen = messageLen + smolcertLen; 
   const uint8_t dummyPubkey[] = { 0x66 ,0x82 ,0x79 ,0x97 ,0x37 ,0xB7 ,0x6C ,0x17 , \
                                   0xC3 ,0x5B ,0x95 ,0x57 ,0x44 ,0x9A ,0x86 ,0x22 , \
                                   0xA7 ,0xB8 ,0xA5 ,0x65 ,0x5C ,0xB3 ,0x85 ,0x1C , \
                                   0x74 ,0x4A ,0xFD ,0x69 ,0xEC ,0x95 ,0x9E ,0x29};
 
-  testMsg.msgBuf = (uint8_t*)malloc((size_t)totalLen+2);
+  testMsg.msgBuf = (uint8_t*)malloc((size_t)totalLen+3);
   testMsg.msgLen = totalLen+2;
-  testMsg.msgBuf[0] = totalLen&0xFF;
-  testMsg.msgBuf[1] = (totalLen&0xFF00 )>>8;
-  testMsg.msgBuf[2] = RESPONSE_PACKET_VERSION;
-  testMsg.msgBuf[3] = RESPONSE_PACKET_TYPE;
-  memcpy((uint8_t*)&(testMsg.msgBuf[4]),dummyPubkey,32);
+  testMsg.msgBuf[0] = RESPONSE_PACKET_TYPE;
+  testMsg.msgBuf[1] = totalLen&0xFF;
+  testMsg.msgBuf[2] = (totalLen&0xFF00 )>>8;
+  
+  memcpy((uint8_t*)&(testMsg.msgBuf[3]),dummyPubkey,32);
+  testMsg.msgBuf[35] = smolcertLen&0xFF;
+  testMsg.msgBuf[36] = (smolcertLen&0xFF00)>>8;
 
-  for(uint8_t rIdx = 0; rIdx < payloadLen; rIdx++){
-    testMsg.msgBuf[36+rIdx] = (uint8_t)rand();
+  for(uint8_t rIdx = 0; rIdx < smolcertLen; rIdx++){
+    testMsg.msgBuf[37+rIdx] = (uint8_t)rand();
   }
 
   err = unpackHandshakeResponse(&testPacket, &testMsg);
@@ -105,8 +114,8 @@ void test_unpackHandshakeResponse(void){
   TEST_ASSERT_EQUAL_MESSAGE(RESPONSE_PACKET_TYPE,testPacket.HandshakeType, "Failed to parse messagetype");
   TEST_ASSERT_EQUAL_HEX8_ARRAY_MESSAGE(&dummyPubkey,testPacket.ephemeralPubKey,32,"Failed to parse ephemeral public key");
 
-  TEST_ASSERT_EQUAL_MESSAGE(payloadLen,testPacket.payloadLen,"Wrong payload length // Failed to parse payload length");
-  TEST_ASSERT_EQUAL_HEX8_ARRAY_MESSAGE(&(testMsg.msgBuf[36]),testPacket.payload,payloadLen,"Failed to parse payload");
+  TEST_ASSERT_EQUAL_MESSAGE(smolcertLen,testPacket.smolcertLen,"Wrong smolcert length // Failed to parse smolcert length");
+  TEST_ASSERT_EQUAL_HEX8_ARRAY_MESSAGE(&(testMsg.msgBuf[37]),testPacket.smolcert,smolcertLen,"Failed to parse smolcert");
 }
 
 
@@ -181,14 +190,16 @@ int main(void) {
     }
     UNITY_BEGIN();
     
+    RUN_TEST(test_readUint16);
     RUN_TEST(test_packHandshakeInit);
     RUN_TEST(test_unpackHandshakeResponse);
     //RUN_TEST(test_packHandshakeFin);
     RUN_TEST(test_makeNoiseHandshake);
+    
 
     return UNITY_END();
     
-    return 0;
+    //return 0;
 }
 
 
