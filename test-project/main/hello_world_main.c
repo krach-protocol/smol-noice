9594d9f74daa949ddc4ed5218027268550a9b076
Till Klocke <till.klocke@gmail.com>
2021-07-23 12:55:30 +0200

    smol-noice should now link against object files of noise-c and the test project against smol-noice, but the freeRTOS port is not complete yet

--- test-project/main/hello_world_main.c
+++ test-project/main/hello_world_main.c
@@ -13,31 +13,17 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+#include <sodium.h>
+#include "handshake.h"
+#include "sc_packet.h"
+#include "sc_err.h"
+
 void app_main(void)
 {
-    printf("Hello world!\n");
-
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU cores, WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Free heap: %d\n", esp_get_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    // Currently this won't execute in a meaningful way, this is to test the build process
+    smolcert_t *clientCert = (smolcert_t*)malloc(sizeof(smolcert_t));
+    sn_buffer_t clientCertBuffer;
+    sn_buffer_t rootCertBuffer;
+    const char* host = "127.0.0.1";
+    sc_init(&clientCertBuffer,&rootCertBuffer,NULL,NULL,host,9095);
 }
