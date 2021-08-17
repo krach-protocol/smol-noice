ffe2acb63484a6bd175884f40c59bd9297cf2e03
Till Klocke <till.klocke@gmail.com>
2021-08-17 16:11:19 +0200

    Fixed function signature

--- test-project/main/hello_world_main.c
+++ test-project/main/hello_world_main.c
@@ -31,9 +31,9 @@ sc_err_t clientCb(uint8_t* data, uint8_t len){
     return SC_OK;
 }
 
-sc_err_t remoteCertCb(uint8_t* data, uint8_t len);
+sc_err_t remoteCertCb(uint8_t* data, uint8_t len, smolcert_t* remoteCert);
 
-sc_err_t remoteCertCb(uint8_t* data, uint8_t len,smolcert_t* remoteCert){
+sc_err_t remoteCertCb(uint8_t* data, uint8_t len, smolcert_t* remoteCert){
     for(uint8_t idx = 0; idx < len; idx++){
         printf("%c",data[idx]);
     }
