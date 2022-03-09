f7682a5f29f576e42469b8879bc216709b35ea50
Till Klocke <till.klocke@gmail.com>
2022-03-09 14:34:55 +0100

    Build a more usable buffer solution, which makes handling bytes buffer easier. But this breaks like everything

--- inc/sn_msg.h
+++ inc/sn_msg.h
@@ -3,16 +3,64 @@
 
 
 #include <inttypes.h>
-#include "sc_err.h"
+#include "sn_err.h"
+
 typedef struct{
-    uint16_t msgLen;
-    uint8_t* msgBuf;
+    uint8_t* _orig_ptr;
+    uint8_t* idx;
+    size_t _cap;
+    size_t len;
 } sn_msg_t,sn_buffer_t;
 
+/**
+ * @brief Creates a new buffer structure with the specified capacity
+ * 
+ * @param _cap capacity of the buffer in bytes
+ * @return sn_buffer_t* 
+ */
+sn_buffer_t* sn_buffer_new(size_t _cap);
+/**
+ * @brief Frees an existing buffer.
+ * 
+ * @param buf 
+ */
+void sn_buffer_free(sn_buffer_t* buf);
+/**
+ * @brief Resets an existing buffer for reuse. This does not null the underlying memory!
+ * 
+ * @param buf 
+ */
+void sn_buffer_reset(sn_buffer_t* buf);
+/**
+ * @brief Resizes an existing buffer to the new capacity. If new_len is smaller than the old
+ * capacity this will lead to data loss. Existing data is kept for all elements which still 
+ * fit into the new capacity
+ * 
+ * @param buf 
+ * @param new_len 
+ */
+void sn_buffer_resize(sn_buffer_t* buf, size_t new_len);
+/**
+ * @brief Copies data into the given buffer by appending it to the end. This will automatically
+ * resize the given buffer to fit all elements
+ * 
+ * @param buf 
+ * @param in_buf 
+ * @param len 
+ */
+void sn_buffer_copy_into(sn_buffer_t *buf, uint8_t* in_buf, size_t len);
+
+/**
+ * @brief Ensure that the given buffer has at least capacity for the expected amount of data
+ * 
+ * @param buf 
+ * @param expected_cap 
+ */
+void sn_buffer_ensure_cap(sn_buffer_t *buf, size_t expected_cap);
 
 //Padding
-sc_err_t padBuffer(sn_buffer_t*);
-sc_err_t unpadBuffer(sn_buffer_t*);
+sn_err_t padBuffer(sn_buffer_t*);
+sn_err_t unpadBuffer(sn_buffer_t*);
 
 
 #endif
--- src/sn_msg.c
+++ src/sn_msg.c
@@ -3,33 +3,73 @@
 #include <stdlib.h>
 #include <string.h>
 
-sc_err_t padBuffer(sn_buffer_t* buffer){
-    uint8_t bufferLen = buffer->msgLen;
-    uint8_t paddedBytes = (16-((bufferLen+16+1)%16));
-    uint8_t newLen = paddedBytes+bufferLen+16+1;
-    uint8_t *newBuffer = (uint8_t*)calloc(1,newLen);
+sn_buffer_t* sn_buffer_new(size_t _cap){
+    sn_buffer_t* buf = (sn_buffer_t*)malloc(sizeof(sn_buffer_t));
+    buf->_orig_ptr = (uint8_t*)malloc(_cap);
+    buf->idx = buf->_orig_ptr;
+    buf->_cap = _cap;
+    buf->len = 0;
+    return buf;
+}
 
-    if(newBuffer == NULL) return SC_ERR;
-    buffer->msgLen = newLen;
+void sn_buffer_free(sn_buffer_t* buf) {
+    free(buf->_orig_ptr);
+    free(buf);
+}
 
-    for(uint8_t idx = 0;idx<bufferLen;idx++){
-        newBuffer[idx+1] = buffer->msgBuf[idx];
+void sn_buffer_reset(sn_buffer_t* buf) {
+    buf->idx = buf->_orig_ptr;
+    buf->len = 0;
+}
+
+void sn_buffer_resize(sn_buffer_t* buf, size_t new_len) {
+    size_t idx_offset = buf->idx - buf->_orig_ptr;
+    buf->_orig_ptr = realloc(buf->_orig_ptr, new_len);
+    if(idx_offset >= new_len) {
+        // If we reduce the buffer size, the current index maybe outside of the
+        // new capacity, therefore we set the offset to 0 to point idx to the beginning 
+        // of the buffer
+        idx_offset = 0;
     }
-    newBuffer[0]=paddedBytes;    
+    buf->idx = buf->_orig_ptr + idx_offset;
+    buf->_cap = new_len;
+    if((idx_offset + buf->len) > new_len) {
+        buf->len = new_len - idx_offset;
+    }
+}
 
-    free(buffer->msgBuf);
-    buffer->msgBuf = newBuffer;
+void sn_buffer_copy_into(sn_buffer_t *buf, uint8_t* in_buf, size_t len) {
+    if((buf->_cap - buf->len) < len) {
+        sn_buffer_resize(buf, (len-(buf->_cap - buf->len)));
+    }
+    memcpy((buf->idx+buf->len), in_buf, len);
+}
+
+void sn_buffer_ensure_cap(sn_buffer_t *buf, size_t expected_cap) {
+    if((buf->_cap - (buf->idx - buf->_orig_ptr)) < expected_cap) {
+        size_t cap_needed = expected_cap - (buf->_cap - (buf->idx - buf->_orig_ptr));
+        size_t new_cap = buf->_cap + cap_needed;
+        sn_buffer_resize(buf, new_cap);
+    }
+}
+
+sc_err_t padBuffer(sn_buffer_t* buf){
+    uint8_t bytes_to_pad = (uint8_t)(buf->len+1)%16;
+    size_t new_len = buf->len + 1 /*pad header */ + bytes_to_pad;
+    sn_buffer_ensure_cap(buf, new_len);
+    // TODO this needs to be optimized. Move every element one to the right
+    for(size_t i = buf->len+1; i>0; i--) {
+        buf->idx[i] = buf->idx[i-1];
+    }
+    buf->idx[0] = bytes_to_pad;
 
     return SC_OK;
-}
-sc_err_t unpadBuffer(sn_buffer_t* buffer){
-    uint8_t bufferLen = buffer->msgLen;
-    uint8_t paddedBytes = buffer->msgBuf[0];
-     for(uint8_t idx = 0;idx<bufferLen-(16+paddedBytes);idx++){
-        buffer->msgBuf[idx] = buffer->msgBuf[idx+1];
-    }
-    buffer->msgBuf[bufferLen-(16+paddedBytes)] = '\0';
 
+}
+sc_err_t unpadBuffer(sn_buffer_t* buf){
+    uint8_t padded_bytes = buf->idx[0];
+    buf->len = buf->len - 1 /*padding header*/ - padded_bytes;
+    buf->idx = buf->idx + 1;
 
     return SC_OK;
 }
